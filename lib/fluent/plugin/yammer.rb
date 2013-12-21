b90d4e001cc38869756062ea70c9edde2150cb29
Shinohara Teruki <ts_3156@yahoo.co.jp>
2013-12-21 15:40:05 +0000

    first commit

---
+++ lib/fluent/plugin/yammer.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/yammer/version"
+
+module Fluent
+  module Plugin
+    module Yammer
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/yammer/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Yammer
+      VERSION = "0.0.1"
+    end
+  end
+end
